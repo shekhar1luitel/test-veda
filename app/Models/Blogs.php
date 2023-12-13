864796e29a7bc3c7d96fc959447aa95c1181f9a3
Shekhar Luitel <shekhar1luitel@gmail.com>
2023-12-13 17:43:45 +0545

    issue fixed

--- app/Models/Blogs.php
+++ app/Models/Blogs.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Blogs extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'name',
+        'detail',
+        'image',
+    ];
 }
